795273c26c2e4e0b941cd4b276a6e84c03135122
Cyberpunk207789 <robloxfan0008885@gmail.com>
2023-08-27 20:14:44 +0300

    Homework for Lesson #5

--- app_advertisements/urls.py
+++ app_advertisements/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import basic_view
 
 urlpatterns = [
-    path('', index)
+    path('', basic_view("index"), name = 'main-page'),
+    path('top-sellers/', basic_view("top-sellers"), name = 'top-sellers'),
+    path('advertisement-post/', basic_view("advertisement-post"), name = 'advertisement-post'),
+    path('register/', basic_view("register"), name = 'register'),
+    path('login/', basic_view("login"), name = 'login'),
+    path('profile/', basic_view("profile"), name = 'profile')
 ]
+
--- app_advertisements/views.py
+++ app_advertisements/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
-def index(request):
-    return HttpResponse("SUCCESS!")
+# Create your views here.
+template_files = {
+    "index": "index.html",
+    "top-sellers": "top-sellers.html",
+    "advertisement-post": "advertisement-post.html",
+    "register": "register.html",
+    "login": "login.html",
+    "profile": "profile.html"
+}
+
+def basic_view(name) -> (...):
+    def function (request):
+        return render(request, template_files[name])
+
+    return function
