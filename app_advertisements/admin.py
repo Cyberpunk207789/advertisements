582b18c644c50709c7f2b9bcfab17ae82dbda845
Cyberpunk207789 <robloxfan0008885@gmail.com>
2023-08-29 15:40:23 +0300

    Homework for lesson # 8

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Advertisement
 
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date']
+    list_filter = ['auction', 'created_at']
+
+    actions = ['disable_auctions', 'enable_auctions']
+
+    @admin.action(description='Убрать возможность торга')
+    def disable_auctions (self, request, queryset):
+        queryset.update(auction = False)
+
+    @admin.action(description='Вернуть возможность торга')
+    def enable_auctions (self, request, queryset):
+        queryset.update(auction = True)
+
+    fieldsets = (
+        ('Общее', 
+            {
+            'fields': ('title', 'description'),
+            }
+        ),
+
+        ('Финансы',
+            {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+            }
+        )
+    )
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
 # Register your models here.
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 # Create your models here.
 
@@ -16,3 +18,21 @@ class Advertisement (models.Model):
     def __str__ (self):
         return f"<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>."
 
+    @admin.display(description = 'дата создания')
+    def created_date(self):
+        from django.utils import timezone
+
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                "<span style = 'color:green; font-weight: bold;'> Сегодня в {} </span>", created_time
+            )
+
+        if self.updated_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                "<span style = 'color:orange; font-weight: bold; font-style: italic;'> Сегодня в {} </span>", created_time
+            )
+        
+        
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
